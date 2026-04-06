dd11b5b53ec168c697daa7f76463e19358261ca9
juandiegogalindo <jdgalindo6@gmail.com>
2026-04-06 15:22:30 -0500

    Subir proyecto a gitHub

---
+++ src/java/com/jdgalindo/dao/StudentDao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jdgalindo.dao;
+
+import com.jdgalindo.model.Student;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jdgal
+ */
+@Stateless
+public class StudentDao implements StudentDaoLocal {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void addStudent(Student student) {
+        em.persist(student);
+    }
+
+    @Override
+    public void editStudent(Student student) {
+        em.merge(student);
+    }
+
+    @Override
+    public void deleteStudent(int studentId) {
+        Student student = em.find(Student.class, studentId);
+        if (student != null) {
+            em.remove(student);
+        }
+    }
+
+    @Override
+    public Student getStudent(int studentId) {
+        return em.find(Student.class, studentId);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return em.createNamedQuery("Student.getAll", Student.class).getResultList();
+    }
+
+}
